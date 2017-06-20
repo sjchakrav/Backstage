071d9952589b9abdbacf3248c4279a8058c30b4c
Sanjay Chakravorty <schakravorty@learnvest.com>
2017-06-20 01:50:53 -0400

    update ETL script -> clean up functions and organize code and update readme

--- ETL.py
+++ ETL.py
@@ -10,30 +10,38 @@ import requests
 from __future__ import print_function
 from StringIO import StringIO
 import sys
+from datetime import datetime
+from __future__ import division
 
-"""request url and retrieve JSON data and normalize data"""
+# request url and retrieve JSON data and normalize data#
+
 
 def load_json_data(url):
 	response = urllib.urlopen(url)
 	data = json.loads(response.read())
 	return data
 
-#normalize json data structure and store into a pandas dataframe
+
+# normalize json data structure and store into a pandas dataframe
 def normalize_data(data):
 	df = json_normalize(data)
 	df.columns = map(unicode.lower, df.columns)
 	return df
 
+
 def write_to_csv(df, file_name):
 	df.to_csv(file_name, encoding='utf-8', index=True, index_label='id')
+
 
 def build_schema(sql_command, engine):
 	with engine.connect() as conn:
 		conn.execute(sql_command)
 
+
 def chunks(l, n):
-    for i in range(0, len(l), n):
-        yield l[i:i+n]
+	for i in range(0, len(l), n):
+		yield l[i:i+n]
+
 
 def print_all_unique_names():
 	base_url = "https://api.genderize.io/?"
@@ -53,6 +61,7 @@ def get_print_out():
 	split = output_string.split('h')
 	return split
 
+
 def normalize_json_response(url):
 	response = requests.get(url)
 	data = json_normalize(response.json())
@@ -68,40 +77,34 @@ def generate_url_and_retrieve_data(split):
 			appended_data.append(data)	
 	appended_data = pd.concat(appended_data, axis=0)
 	appended_data.reset_index(drop=True)
-	appended_data = appended_data.rename(columns={'name':'first_names'})
+	appended_data = appended_data.rename(columns={'name': 'first_names'})
 	return appended_data
+
+
+df['date_of_death'] = df.apply(lambda x: pd.datetime.strptime("{0} {1} {2} 00:00:00".format(x['year_date_of_death'],x['month_date_of_death'], x['day_date_of_death']), "%Y %m %d %H:%M:%S"),axis=1)
 
 
 def main():
 	data = load_json_data("http://www.veterans.gc.ca/xml/jsonp/app.cfc?method=remoteGetAllCasualtyInfo&start=1&end=500&callback=?&language=en")
 	df = normalize_data(data['casualties'])
-
-	#establish connection object to hosted pg db
-	engine = create_engine('postgresql://root:backstagetesting123456789@backstageinstance.ctjtojl7pj7i.us-west-2.rds.amazonaws.com:5432/prod')
-	#build schema
-	sql_command = open('/Users/schakravorty/Development/Backstage/schema.sql', 'r').read()
-	build_schema(sql_command, engine)
-
-	#process via genderize's API
-	#extract only the first word from each string for easy mapping
+	# process via genderize's API
+	# extract only the first word from each string for easy mapping
 	df['first_names'] = df['forenames'].apply(lambda x: x.split(' ', 1)[0])
-	#determine the distinct names
+	# determine the distinct names
 	unique_names = df['first_names'].unique()
 	chunked_list = list(chunks(unique_names, 10))
 	split = get_print_out()
 	gender_data = generate_url_and_retrieve_data(split)
-
-	"""join data with pd.merge on 'first_names'"""
-
+	# join data with pd.merge on 'first_names'
 	merged_data = pd.merge(df, gender_data, on='first_names', how='inner')
 	merged_data['count'] = merged_data['count'].fillna(0.0).astype(int)
-
 	write_to_csv(merged_data, 'canadian_memorial.csv')
 
-	"""load data via psql terminal commands"""
-	#psql --host=backstageinstance.ctjtojl7pj7i.us-west-2.rds.amazonaws.com --port=5432 --username=root --dbname=prod --password
-	#\COPY canadian_war_memorial FROM '~/Development/Backstage/canadian_memorial.csv'  CSV HEADER
-	#COPY 499
+	# establish connection object to hosted pg db
+	engine = create_engine('postgresql://root:backstagetesting123456789@backstageinstance.ctjtojl7pj7i.us-west-2.rds.amazonaws.com:5432/prod')
+	# build schema
+	sql_command = open('/Users/schakravorty/Development/Backstage/schema.sql', 'r').read()
+	build_schema(sql_command, engine)
 
 if __name__ == "__main__":
 	main()
--- README.md
+++ README.md
@@ -1,8 +1,10 @@
 To install the requirements for this python script, simple run: pip install -r requirements.txt
 
-This python script utilizes pandas and retrieves data from the Canadian virtual war memorial, normalizes the JSON structure, and processes the data through use of the genderize.io API and saves the data into a CSV format for easy access via pandas or another analytics library that can read in data in a CSV format.
+To run the script: python2.7 ETL.py
 
-I've also hosted a free-tier postgres database instance on AWS RDS along with a read replica, with a read-only user for easy querability via SQL.
+This python script utilizes pandas and retrieves data from the Canadian virtual war memorial, normalizes the JSON structure, and processes the data through use of the genderize.io API and saves the data into a CSV format for easy access via pandas or another analytics library that can read in data in a CSV format. 
+
+I've also hosted a free-tier postgres database instance on AWS RDS along with a read replica, with a read-only user for easy querability via SQL. The python script also establishes a connection to the database and builds the schema (schema.sql).
 
 -Copied the CSV directly into the database via psql:
 
@@ -14,3 +16,26 @@ host = 'backstageinstance.ctjtojl7pj7i.us-west-2.rds.amazonaws.com'
 port = '5432'
 database = 'prod'
 
+
+- Of Canadian names that were considered masculine in various years in the past, what percentage are now gender-ambiguous?
+
+- Which branches of the Canadian military forces have employed the most men whose names are now gender-ambiguous?
+
+- What percentage of Canadian men died on the same day of the month as they were born?
+	>>> len(df[(df['day_date_of_death']==df['day_date_of_birth'])])/len(df)*100
+	# 1.8036072144288577
+
+	OR
+
+SELECT y.same_day, x.total, (y.same_day::float/x.total::float*100) as percentage
+FROM
+(
+	SELECT COUNT(id) as same_day
+	FROM canadian_war_memorial
+	WHERE day_date_of_death = day_date_of_birth
+) y
+CROSS JOIN
+(
+	SELECT COUNT(id) as total
+	FROM canadian_war_memorial
+) x;
